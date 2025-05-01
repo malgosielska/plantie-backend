004145c5aa287e66fd3c83bc3244b1f65929ba99
malgosielska <malgosielska@gmail.com>
2025-05-01 19:42:38 +0200

    added treatment to API response

--- app/routers/predict.py
+++ app/routers/predict.py
@@ -5,6 +5,8 @@ from fastapi.responses import JSONResponse
 from PIL import Image
 import io
 import torch
+from app.utils.managements import managements
+from app.utils.symptoms import symptoms
 from app.utils.constants import CLASS_INDICES_PATH, CLASSES_COUNT, MODEL_PATH
 from app.model.model_loader import load_class_indices, load_model
 from app.utils.images import load_image
@@ -27,7 +29,17 @@ async def predict_image(file: UploadFile = File(...)):
             outputs = model(tensor)
             _, predicted_idx = torch.max(outputs, 1)
             predicted_class = class_indices[predicted_idx.item()]
-        return JSONResponse(content={"predicted_class": predicted_class})
+            predicted_symptoms = symptoms.get(predicted_class, "No symptoms available.")
+            predicted_management = managements.get(
+                predicted_class, "No managements available."
+            )
+        return JSONResponse(
+            content={
+                "predicted_class": predicted_class,
+                "symptoms": predicted_symptoms,
+                "management": predicted_management,
+            }
+        )
 
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
