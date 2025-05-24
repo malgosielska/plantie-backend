d27870b94892e64a69d8f02a36a437c6d0cc159f
izabela <iza.kalenik@gmail.com>
2025-05-24 14:29:47 +0200

    names and description updated

---
+++ app/utils/class_names.py
@@ -0,0 +1,56 @@
+
+class_names = {
+    "Apple___Apple_scab": "Apple - Apple Scab",
+    "Apple___Black_rot": "Apple - Black Rot",
+    "Apple___Cedar_apple_rust": "Apple - Cedar Apple Rust",
+    "Apple___healthy": "Apple - Healthy",
+
+    "Blueberry___healthy": "Blueberry - Healthy",
+
+    "Cherry_(including_sour)___Powdery_mildew": "Cherry - Powdery Mildew",
+    "Cherry_(including_sour)___healthy": "Cherry - Healthy",
+
+    "Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot": "Corn - Gray Leaf Spot (Cercospora)",
+    "Corn_(maize)___Common_rust_": "Corn - Common Rust",
+    "Corn_(maize)___Northern_Leaf_Blight": "Corn - Northern Leaf Blight",
+    "Corn_(maize)___healthy": "Corn - Healthy",
+
+    "Grape___Black_rot": "Grape - Black Rot",
+    "Grape___Esca_(Black_Measles)": "Grape - Esca (Black Measles)",
+    "Grape___Leaf_blight_(Isariopsis_Leaf_Spot)": "Grape - Leaf Blight (Isariopsis)",
+    "Grape___healthy": "Grape - Healthy",
+
+    "Orange___Haunglongbing_(Citrus_greening)": "Orange - Citrus Greening (Huanglongbing)",
+
+    "Peach___Bacterial_spot": "Peach - Bacterial Spot",
+    "Peach___healthy": "Peach - Healthy",
+
+    "Pepper,_bell___Bacterial_spot": "Bell Pepper - Bacterial Spot",
+    "Pepper,_bell___healthy": "Bell Pepper - Healthy",
+
+    "Potato___Early_blight": "Potato - Early Blight",
+    "Potato___Late_blight": "Potato - Late Blight",
+    "Potato___healthy": "Potato - Healthy",
+
+    "Raspberry___healthy": "Raspberry - Healthy",
+
+    "Soybean___healthy": "Soybean - Healthy",
+
+    "Squash___Powdery_mildew": "Squash - Powdery Mildew",
+
+    "Strawberry___Leaf_scorch": "Strawberry - Leaf Scorch",
+    "Strawberry___healthy": "Strawberry - Healthy",
+
+    "Tomato___Bacterial_spot": "Tomato - Bacterial Spot",
+    "Tomato___Early_blight": "Tomato - Early Blight",
+    "Tomato___Late_blight": "Tomato - Late Blight",
+    "Tomato___Leaf_Mold": "Tomato - Leaf Mold",
+    "Tomato___Septoria_leaf_spot": "Tomato - Septoria Leaf Spot",
+    "Tomato___Spider_mites Two-spotted_spider_mite": "Tomato - Two-Spotted Spider Mite",
+    "Tomato___Target_Spot": "Tomato - Target Spot",
+    "Tomato___Tomato_Yellow_Leaf_Curl_Virus": "Tomato - Yellow Leaf Curl Virus",
+    "Tomato___Tomato_mosaic_virus": "Tomato - Mosaic Virus",
+    "Tomato___healthy": "Tomato - Healthy",
+}
+
+
